70022757b758c07d9280ecbd2ca24e87c0eef87f
David Hafen <dhafen21@gmail.com>
2020-04-26 09:20:08 -0600

    search two urls

--- Sheet.py
+++ Sheet.py
@@ -28,7 +28,7 @@ class Sheet:
                 #   print("bad url: {}".format(j))
                 #   self.no_url.append(sheet.cell_value(i,1))
                 #   # print("unable to find url for {}".format(sheet.cell_value(i,1)))
-            if !found:
+            if found == false:
                 self.no_url.append(sheet.cell_value(i,1))
         return self.url_array
 
