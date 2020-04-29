5064ece1cad1f79d03d83e298311bbe5288bba46
David Hafen <dhafen21@gmail.com>
2020-04-29 14:06:08 -0600

    Increased Pause Limit

--- Sheet.py
+++ Sheet.py
@@ -25,15 +25,16 @@ class Sheet:
             string = "{} {} {} rate my professor".format(a[1], a[0], self.school_name)
             # string = "{} {} rate my professor".format(sheet.cell_value(i,0), self.school_name)
             @retry(wait_fixed=2000)
-            for j in search(string, 'co.in', num = 1, stop = 1, pause = .7):
-                if "ratemyprofessors.com/ShowRatings" in j:
-                  self.url_array.append(j)
-                  self.name_array.append(sheet.cell_value(i,0))
-                  break
-                else:
-                  print("bad url: {}".format(j))
-                  self.no_url.append(sheet.cell_value(i,0))
-                  # print("unable to find url for {}".format(sheet.cell_value(i,1)))
+            def do_stuff:
+                for j in search(string, 'co.in', num = 1, stop = 1, pause = .7):
+                    if "ratemyprofessors.com/ShowRatings" in j:
+                      self.url_array.append(j)
+                      self.name_array.append(sheet.cell_value(i,0))
+                      break
+                    else:
+                      print("bad url: {}".format(j))
+                      self.no_url.append(sheet.cell_value(i,0))
+                      # print("unable to find url for {}".format(sheet.cell_value(i,1)))
         return self.url_array
 
     def add_to_workbook(self, prof):
