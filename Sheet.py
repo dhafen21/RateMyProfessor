81eb8b7a0073888f947a6c4137703529a0a61854
David Hafen <dhafen21@gmail.com>
2020-05-01 14:12:38 -0600

    Increased pause btween searches

--- Sheet.py
+++ Sheet.py
@@ -29,7 +29,7 @@ class Sheet:
             a = sheet.cell_value(i,0).split(", ")
             string = "{} {} {} rate my professor".format(a[1], a[0], self.school_name)
             # string = "{} {} rate my professor".format(sheet.cell_value(i,0), self.school_name
-            for j in search(string, 'co.in', num = 1, stop = 1, pause = 1):
+            for j in search(string, 'co.in', num = 1, stop = 1, pause = 1.5):
                 print(index)
                 if "ratemyprofessors.com/ShowRatings" in j:
                   self.url_array.append(j)
