73ce0f3b5b4c91e86948109a4118b3f2271a805c
David Hafen <dhafen21@gmail.com>
2020-05-01 13:07:29 -0600

    Changed back

--- Sheet.py
+++ Sheet.py
@@ -23,8 +23,8 @@ class Sheet:
             # print(sheet.cell_value(i,0))
             a = sheet.cell_value(i,0).split(", ")
             string = "{} {} {} rate my professor".format(a[1], a[0], self.school_name)
-            # string = "{} {} rate my professor".format(sheet.cell_value(i,0), self.school_name)
-            for j in search(string, 'co.in', num = 1, stop = 1, pause = .7):
+            # string = "{} {} rate my professor".format(sheet.cell_value(i,0), self.school_name
+            for j in search(string, 'co.in', num = 1, stop = 1, pause = .01):
                 print("2")
                 if "ratemyprofessors.com/ShowRatings" in j:
                   self.url_array.append(j)
