7b97df71335ad11a2afdeea0a1dcbd29e60b839e
David Hafen <dhafen21@gmail.com>
2020-05-13 16:41:12 -0600

    Updated Xpath for professor First and Last name

--- scrape.py
+++ scrape.py
@@ -67,7 +67,10 @@ def scrape(url, driver):
         print("Unable to find the class name for {}".format(url))
 
     try:
-        prof_name = driver.find_element_by_xpath("//div[@class = 'NameTitle__Name-dowf0z-0 cjgLEI']").text
+        prof_first_name = driver.find_element_by_xpath("//div[@class = 'NameTitle__Name-dowf0z-0 jeLOXk']").text
+        prof_last_name = driver.find_element_by_xpath("//div[@class = 'NameTitle__LastNameWrapper-dowf0z-2 glXOHH']").text
+        prof_name = "{} {}".format(prof_first_name, prof_last_name)
+
     except:
         prof_name = "Error Error"
         print("Unable to find the professor name for {}".format(url))
