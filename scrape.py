2445e0f0345776ec29bc6d2aa02bd898565a8baa
David Hafen <dhafen21@gmail.com>
2020-05-13 20:39:44 -0600

    Added number of iterations check:

--- scrape.py
+++ scrape.py
@@ -20,11 +20,13 @@ def scrape(url, driver):
     except:
         print("There was no 'agree to cookies' box")
 
+    iter = 0
     while True:
         try:
             button = driver.find_element_by_class_name("fwhQq")
             button.click()
-            print("clicked the button")
+            iter +=10
+            print("number added: {}".format(iter))
             time.sleep(.25)
         except:
             # print("Done clicking buttons")
