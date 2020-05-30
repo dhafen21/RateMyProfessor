208b2edc04c73f97c244d201d288a1e9ff688c29
David Hafen <dhafen21@gmail.com>
2020-05-29 19:25:50 -0600

    Added the dates

--- Professor.py
+++ Professor.py
@@ -9,7 +9,7 @@ class Professor:
         self.data = Responses.Responses()
 
     def get_data(self, url, wd):
-        self.data.class_name, self.data.quality, self.data.difficulty, self.data.comments, self.name, self.date = scrape(self.url, wd)
+        self.data.class_name, self.data.quality, self.data.difficulty, self.data.comments, self.name, self.data.date = scrape(self.url, wd)
 
     def set_url(self, url):
         self.url = url
