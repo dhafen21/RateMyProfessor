434a0e656431b8bcc724950c37ea742b76cf08af
David Hafen <dhafen21@gmail.com>
2020-05-29 19:15:32 -0600

    Added the dates

--- scrape.py
+++ scrape.py
@@ -71,7 +71,7 @@ def scrape(url, driver):
         print("Unable to find the class name for {}".format(url))
 
     try:
-        for i in wd.find_elements_by_xpath("//div[@class = 'TimeStamp__StyledTimeStamp-sc-9q2r30-0 bXQmMr RatingHeader__RatingTimeStamp-sc-1dlkqw1-3 BlaCV']"):
+        for i in driver.find_elements_by_xpath("//div[@class = 'TimeStamp__StyledTimeStamp-sc-9q2r30-0 bXQmMr RatingHeader__RatingTimeStamp-sc-1dlkqw1-3 BlaCV']"):
             print(i.text)
             dates.append(i.text)
     except:
